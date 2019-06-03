7d8c4d94e172f363fa9adfc7ef5b939e00871d1a
kmayur416 <41637523+kmayur416@users.noreply.github.com>
2019-06-03 15:46:29 +0530

    hackerrank if else
    Task 
    Given an integer, , perform the following conditional actions:
    
    If n is odd, print Weird
    If n is even and in the inclusive range of 2 to 5 , print Not Weird
    If n is even and in the inclusive range of  6 to 20 , print Weird
    If n is even and greater than 20 , print Not Weird

---
+++ aed.py
@@ -0,0 +1,12 @@
+n=int(input("Enter number n "))
+if (n%2!=0): 
+    print("weird")
+elif (n%2==0) and (2<=n<=5) or (n>20):
+        
+    
+    
+        print("not weird")
+else:
+    (n%2==0) and (6<=n<=20)
+    print("weird")
+        
